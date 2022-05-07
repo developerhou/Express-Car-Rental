d578dbf8530a6f1e1e63087272df0260aabcbd45
Xiaojie Hou <13233912810@163.com>
2022-05-07 22:19:44 +1000

    Initial project

---
+++ ExpressCarRental/ExpressCarRental/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpressCarRental
+//
+//  Created by Xiaojie Hou on 7/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpressCarRental/ExpressCarRental/ExpressCarRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressCarRentalApp.swift
+//  ExpressCarRental
+//
+//  Created by Xiaojie Hou on 7/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpressCarRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
